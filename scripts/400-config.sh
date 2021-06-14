8a7b88b819a7026da0efe4d744ae38dc969b6e1a
Jonas <jonas@innomega.se>
2021-06-14 11:11:16 +0200

    Update 400-config.sh

--- scripts/400-config.sh
+++ scripts/400-config.sh
@@ -7,14 +7,14 @@ echo "Adding CommandBox Default as service"
 update-rc.d commandbox-default defaults
 systemctl enable commandbox-default
 
-echo "Add Certbot PPA"
-debconf-apt-progress -- apt install software-properties-common -y
-add-apt-repository universe
-add-apt-repository ppa:certbot/certbot
-debconf-apt-progress -- apt update -y
+#echo "Add Certbot PPA"
+#debconf-apt-progress -- apt install software-properties-common -y
+#add-apt-repository universe
+#add-apt-repository ppa:certbot/certbot
+#debconf-apt-progress -- apt update -y
 
-if [ "$CERTBOT" = "Yes" ]; then
-	echo "Install Certbot"
-	debconf-apt-progress -- apt install certbot python-certbot-nginx -y
-	certbot --nginx
-fi
+#if [ "$CERTBOT" = "Yes" ]; then
+#	echo "Install Certbot"
+#	debconf-apt-progress -- apt install certbot python-certbot-nginx -y
+#	certbot --nginx
+#fi
